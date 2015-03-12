722612a3a12ff58aedaadfd7e951ad6625abb026
Shisei Hanai <ruimo.uno@gmail.com>
2015-03-13 04:29:18 +0900

    Sync timezone.

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 5432:5432 -v /var/pgsql:/var/pgsql:rw --name postgres ruimo/df-ub1404-pgsql935 $*
+docker run -d -p 5432:5432 -v /var/pgsql:/var/pgsql:rw -v /etc/localtime:/etc/localtime:ro --name postgres ruimo/df-ub1404-pgsql935 $*
